5de015586bcac9ae081924cff60eafb1d9c98d3c
Konstantin Frank <konstantin@Konstantins-MacBook-Pro.local>
2019-05-19 22:57:17 +0200

    NavBar added as component and in app

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-white mt-2" style="display: flex; justifyContent: flex-end;">
+            <a class="navbar-item mr-5" href="#">Article</a>
+            <a class="navbar-item mr-4" href="#">Contact</a>
+        </nav>   
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
