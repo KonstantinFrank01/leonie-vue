1f740ed5654713fd81a5270dbff6c5fe43111c79
Konstantin Frank <konstantin@Konstantins-MBP.mshome.net>
2019-06-03 13:10:54 +0200

    Article Paths added

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
         excerptHeader="Schlossmuseum: Blick in technologische Zukunft"
         excerpt="Die Besucher lernen Leonie kennen - eine Sprachassistentin, die neben einer Stimme auch einen digitalen Körper hat. Leonie kann nicht nur auf Kommando tanzen, sondern auch Fragen beantworten, etwa über die technologischen Innovationen, die auf uns zukommen, erklärt der Maturant Karsten Köhne. In der HTL Leonding wurde Leonie von Schülern entwickelt."
         imgPath="code.jpeg"
+        articlePath="https://ooe.orf.at/news/stories/2957975/?fbclid=IwAR1woeeBFuToOVibSdHlwoRkSXZEZcKXG6wphbcmq9WR9W-F9zebeUEeqo4"
       />
 
       <Article
@@ -18,6 +19,7 @@
         excerptHeader="Neues aus der Zukunft"
         excerpt="Alexa und Siri kennen Sie vermutlich. Aber kennen Sie auch Leonie? Die Oö. Zukunftsakademie, ein Think Tank der Oö. Landesregierung, blickt in die Zukunft und stellt im Innovationseck der Dauerausstellung „Technik Oberösterreich“ technologische Entwicklungen der nächsten Jahre, Jahrzehnte, vielleicht auch Jahrhunderte vor."
         imgPath="girl.jpeg"
+        articlePath="http://www.landesmuseum.at/de/ausstellungen/detail/techvisionen-neues-aus-der-zukunft.html"
       />
 
       <Article
@@ -27,6 +29,7 @@
         excerptHeader="Welt der Zukunft"
         excerpt="3D-Avatare, ein Gehirn-Computer-Interface, humanoide Roboter, Ganzkörperscanner – die Sonderschau Tech Visionen – Neues aus der Zukunf gibt seit Mittwoch im Linzer Schlossmuseum einen Ausblick auf künftige technologische Entwicklungen. Die in Kooperation mit der Zukunftsakademie – dem Thinktank der Landesregierung – entstandene Schau läuft bis 31. März. Zu sehen ist sie im Innovationseck der Dauerausstellung Technik Oberösterreich."
         imgPath="leonie_first_playcanvas_test.png"
+        articlePath="https://www.nachrichten.at/nachrichten/kultur/das-schlossmuseum-wirft-einen-blick-in-die-welt-der-zukunft;art16,3090778"
       />
 
       <Article
@@ -36,6 +39,7 @@
         excerptHeader="TechVisionen – Neues aus der Zukunft"
         excerpt="Der Avatar Leonie als 3D-Hologramm der HTL Leonding wurde für die Ausstellung trainiert, Fragen zu aktuellen Studien der Zukunftsakademie zu beantworten. Eine Sonderschau TechVisionen – Neues aus der Zukunft gibt seit Mittwoch im Bereich der Technik-Dauerausstellung im Linzer Schlossmuseum einen Ausblick auf künftige Technologien."
         imgPath="meeting.jpeg"
+        articlePath="https://derstandard.at/2000095788518/Neues-aus-der-Zukunft-im-Linzer-Schlossmuseum"
       />
     </section>
 
